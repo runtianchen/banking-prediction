3826714ad36a242472f832e93122f5e8597f9fa2
runtianchen <lightillidan@163.com>
2018-01-25 14:41:11 +0800

    2018.1.25
    basically finish

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # banking-prediction
+
+银行系统通过对用户的电话访谈获取相关信息，以预测该用户是否会在未来办理相关业务。
+
+banking prediction.py 预测模型
+banking.csv 数据库文件
+Figure_1.png 模型性能图示
--- banking prediction.py
+++ banking prediction.py
@@ -29,13 +29,18 @@
 # Output variable (desired target):
 # 21 - y - has the client subscribed a term deposit? (binary: 'yes','no')
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import scale
+from sklearn.model_selection import StratifiedKFold
+from sklearn.metrics import roc_curve, auc
 
 if __name__ == "__main__":
+    # 从banking.csv文件中读取数据
     data = pd.read_csv(r'./banking.csv', sep=',')
-    # print(data.keys())
-    # print(data.shape)
-    # 将数据集中所有object类型对象转换为int64类型对象
+    # 将所有非数值型特征转换为数值型
     for i in data.keys():
         new_columns = {}
         index = 0
@@ -46,5 +51,47 @@ if __name__ == "__main__":
                     new_columns[key] = index
                     index += 1
             data[i] = data[i].map(new_columns)
-    feature = data.drop['y'].values
+    # 将特征值与标签'y'分离
+    feature = data.drop('y', 1).values
     label = data['y'].values
+    # 特征值归一化
+    feature = scale(feature)
+
+    # 使用6折交叉验证，并且画ROC曲线
+    skf = StratifiedKFold(n_splits=6)
+    lg = LogisticRegression()
+
+    mean_tpr = 0.0
+    mean_fpr = np.linspace(0, 1, 100)
+    all_tpr = []
+    k = 0
+
+    for train, test in skf.split(feature, label):
+        lg.fit(feature[train], label[train])
+        predicts = lg.predict_proba(feature[test])
+        fpr, tpr, thresholds = roc_curve(label[test], predicts[:, 1], pos_label=1)
+        roc_auc = auc(fpr, tpr)
+        # 画图，只需要plt.plot(fpr,tpr),变量roc_auc只是记录auc的值，通过auc()函数能计算出来
+        plt.plot(fpr, tpr, lw=1, label='ROC fold %d (AUC = %0.2f)' % (k, roc_auc,))
+        k += 1
+        # 对mean_tpr在mean_fpr处进行插值，通过scipy包调用interp()函数
+        mean_tpr += np.interp(mean_fpr, fpr, tpr)
+    mean_tpr[0] = 0.0  # 初始处为0
+    mean_tpr /= 6  # 在mean_fpr100个点，每个点处插值插值多次取平均
+    mean_tpr[-1] = 1.0  # 坐标最后一个点为（1,1）
+    mean_auc = auc(mean_fpr, mean_tpr)  # 计算平均AUC值
+    # 画平均ROC曲线
+    plt.plot(mean_fpr, mean_tpr, 'k--',
+             label='Mean ROC (AUC = %0.2f)' % mean_auc, lw=2)
+
+    # 画对角线
+    plt.plot([0, 1], [0, 1], '--', color=(0.6, 0.6, 0.6), label='Luck')
+
+    # 图例属性设置
+    plt.xlim([-0.05, 1.05])
+    plt.ylim([-0.05, 1.05])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    # # plt.title('Receiver operating characteristic example')
+    plt.legend(loc="lower right")
+    plt.show()
